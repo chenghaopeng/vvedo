cda57f64a36a2ceaa4f5a59d816242b5b1f509fe
chenghaopeng <i@chper.cn>
2020-07-27 12:48:19 +0800

    第一次提交

---
+++ app/src/main/java/cn/chper/vvedo/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package cn.chper.vvedo.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
